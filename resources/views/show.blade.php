ba5871df5fe8a142c4707b916fe8b373e34cbcf0
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-09 19:39:22 +0200

    Comment CRUD

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,3 +1,5 @@
+<!-- show.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
@@ -6,10 +8,30 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
-                    <p>{{ $post->title }}</p>
+                    <p><b>{{ $post->title }}</b></p>
                     <p>
                         {{ $post->body }}
                     </p>
+                    <hr />
+                    <h4>Display Comments</h4>
+                    @foreach($post->comments as $comment)
+                        <div class="display-comment">
+                            <strong>{{ $comment->user->name }}</strong>
+                            <p>{{ $comment->body }}</p>
+                        </div>
+                    @endforeach
+                    <hr />
+                    <h4>Add comment</h4>
+                    <form method="post" action="{{ route('comment.add') }}">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" name="comment_body" class="form-control" />
+                            <input type="hidden" name="post_id" value="{{ $post->id }}" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-warning" value="Add Comment" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get('/post/create', 'PostController@create')->name('post.create');
 Route::post('/post/store', 'PostController@store')->name('post.store');
 Route::get('/posts', 'PostController@index')->name('posts');
 Route::get('/post/show/{id}', 'PostController@show')->name('post.show');
+Route::post('/comment/store', 'CommentController@store')->name('comment.add');
